037dda4313434464e7811a4b59ced520bf162957
Nicholas Obert <nchsluba@gmail.com>
2021-09-17 21:40:08 +0200

    finished

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from typing import Tuple, NewType
+from typing import List, Tuple, NewType
 from PIL import Image
 
 
@@ -11,11 +11,41 @@ MAX_INTENSITY = 255
 
 
 def map_intensity_to_character(intensity) -> CHARACTERS:
-    return CHARACTERS[intensity * len(CHARACTERS) // MAX_INTENSITY]
+    return CHARACTERS[intensity]
 
 
 def get_pixel_intensity(pixel: Pixel):
-    return sum(pixel) // 1020 # 255 * 4
+    return round(sum(pixel) / 1020 * len(CHARACTERS)) # 1020 = 255 * 4
+
+
+class Printer:
+
+    def __init__(self, size: Tuple[int, int], chars: str) -> None:
+        self.size = size
+        self.chars = chars
+
+    def print(self) -> None:
+        #print(self.chars)
+        index = 0
+        for _ in range(self.size[1]):
+            print(self.chars[index:index+self.size[0]])
+            index += self.size[0]
+
+
+class Counter:
+
+    def __init__(self) -> None:
+        self.nums = {}
+    
+    def count(self, num):
+        if num in self.nums.keys():
+            self.nums[num] += 1
+        else:
+            self.nums[num] = 1
+    
+    def results(self):
+        for key, value in self.nums.items():
+            print(f'Key: {key}, value: {value}')
 
 
 def main() -> None:
@@ -25,13 +55,22 @@ def main() -> None:
 
     print(image.size, image.mode, image.size, image.getcolors())
 
+    counter = Counter()
     
+    chars = ''
     for pixel in image.getdata():
         intensity = get_pixel_intensity(pixel)
         character = map_intensity_to_character(intensity)
-        print(f'{intensity=}, {character=}, {pixel=}', end='\t')
+        chars += character
+        #print(intensity)
+        counter.count(character)
+        #print(character)
+        #print(f'{intensity=}, {character=}, {pixel=}', end='\t')
 
     # image.show()
+    printer = Printer(image.size, chars)
+    printer.print()
+    counter.results()
 
 
 if __name__ == '__main__':
